cef17b39a57ed5151c0ee6e353023fae55e894a9
ZimaBlue1337 <144638087+mjcorp137@users.noreply.github.com>
2023-09-14 19:11:18 +0800

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Prep2 World!");
+        //What is your first name? Scott
+        //What is your last name? Burton
+        //Your name is Burton, Scott Burton.
+        Console.Write($"What is your first name?");
+        string firstName = Console.ReadLine();
+        Console.Write($"What is your last name?");
+        string LastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {LastName}, {firstName} {LastName}")
     }
 }
