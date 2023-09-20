b47585f00cea16e8f7082f33f86d1223b837d9b1
ZimaBlue1337 <144638087+mjcorp137@users.noreply.github.com>
2023-09-20 12:16:23 +0800

    Update Program.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = EnterUserName();
+        int favoriteNumber = EnterFavoriteNumber();
+
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        FinalResult(name, squaredNumber);
+    }
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string EnterUserName(){
+        Console.Write("Please enter your name: ");
+        string fullName = Console.ReadLine();
+
+        return fullName;
+    }
+    static int EnterFavoriteNumber(){
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+
+        return favoriteNumber;
+    }
+    static int SquareNumber(int favoriteNumber){
+        int square = favoriteNumber * favoriteNumber;
+
+        return square;
+    }
+    static void FinalResult(string fullName, int square){
+        Console.WriteLine($"{fullName}, the square of your number is {square}");
     }
 }
