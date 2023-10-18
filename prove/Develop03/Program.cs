6f2fc4846483cd356107eae5bce368342d675dc6
mjcorp137 <savediamahonri@gmail.com>
2023-10-18 16:11:43 +0800

    new

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
-class Program
+namespace DailyScripture
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop03 World!");
+        
+        static void Main()
+        {
+            Scripture scripture = new Scripture("Ether ", 12, 27, "And if men come unto me I will show unto them their aweakness. I bgive unto men weakness that they may be humble; and my cgrace is sufficient for all men that dhumble themselves before me; for if they humble themselves before me, and have faith in me, then will I make eweak things become strong unto them.");
+            Console.Clear();
+            scripture.Display(); 
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+
+                if (!scripture.HideRandomWord())
+                {
+                    Console.WriteLine("\nYou got this, you memorized the scripture!!");
+                    break;
+                }
+
+                Console.Clear();
+                scripture.Display();
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+            }
+        }
     }
 }
